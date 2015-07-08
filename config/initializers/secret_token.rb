4a4455277cd4155d9c6edb54f7c9c4cb9f081dc4
Ahsan Ellahi <blueflame896@gmail.com>
2015-07-08 13:05:41 +0500

    VCS Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitProjectVCS::Application.config.secret_token = 'c95d943ae8de53259f45d477e68e591ebd56d5e4d6ae31e689086c57031bc8ed2c7195b5e45571473c6aa1e1f0be2755e295e79ea88c6b02a0d09b642310af72'
